60dc4cffe69fb5842ca01efb3586acc395e5477d
Lolita <LolitaKardan@yandex.ru>
2022-08-27 22:53:06 +0300

    add a*b

--- Praktikum.java
+++ Praktikum.java
@@ -1,5 +1,7 @@
 public class Praktikum {
     public static void main(String[] args) {
-        System.out.println("Hello, world")
+       int a = 2;
+       int b = 10;
+        System.out.println(a*b);
     }
 }
